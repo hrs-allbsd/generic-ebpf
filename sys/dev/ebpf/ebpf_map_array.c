fdeedc61a67b9bf7b343e5dba791a7bd40958277
Hiroki Sato <hrs@allbsd.org>
2018-08-01 16:30:06 +0000

    - Use ebpf_obj instead of ebpf_map. - Add ebpf_map_array.h for consistency. - Simplify duplicated code.

--- sys/dev/ebpf/ebpf_map_array.c
+++ sys/dev/ebpf/ebpf_map_array.c
@@ -17,271 +17,255 @@
  */
 
 #include "ebpf_map.h"
+#include "ebpf_map_array.h"
 
-struct ebpf_map_array {
-	void *array;
-};
-
-#define ARRAY_MAP(_map) ((struct ebpf_map_array *)(_map->data))
+#define MAP2EMA(m)		((struct ebpf_map_array *)(m))
+#define	BASE_PTR(m)		(MAP2EMA(m)->ma.array)
+#define	BASE_PTR_PERCPU0(m)	(MAP2EMA(m)->ma.parray)
+#define	BASE_PTR_PERCPU(m, c)	(MAP2EMA(m)->ma.parray[c])
+#define	SLOT_PTR(m, base, i)	((uint8_t *)(base) + ((m)->value_size * i))
+#define	SLOT(m, i)		(((m)->value_size * i))
 
 static void
-array_map_deinit(struct ebpf_map *map, void *arg)
+array_map_deinit(struct ebpf_obj *eo, void *arg)
 {
-	struct ebpf_map_array *array_map = map->data;
+	struct ebpf_map *m = EO2EMAP(eo);
 
 	ebpf_epoch_wait();
 
-	ebpf_free(array_map->array);
-	ebpf_free(array_map);
+	ebpf_assert(m != NULL);
+	ebpf_free(BASE_PTR(m));
 }
 
 static void
-array_map_deinit_percpu(struct ebpf_map *map, void *arg)
+array_map_deinit_percpu(struct ebpf_obj *eo, void *arg)
 {
-	struct ebpf_map_array *array_map = map->data;
+	struct ebpf_map *m = EO2EMAP(eo);
 
 	ebpf_epoch_wait();
 
 	for (uint16_t i = 0; i < ebpf_ncpus(); i++) {
-		ebpf_free(array_map[i].array);
+		EBPF_DPRINTF("%s: free %p (m=%p, i=%d)\n",
+		    __func__, BASE_PTR_PERCPU(m, i), m, i); 
+		ebpf_free(BASE_PTR_PERCPU(m, i));
 	}
-
-	ebpf_free(array_map);
+	ebpf_assert(m != NULL);
+	EBPF_DPRINTF("%s: free %p (m=%p)\n", __func__,
+	    BASE_PTR_PERCPU0(m), m); 
+	ebpf_free(BASE_PTR_PERCPU0(m));
 }
 
 static int
-array_map_init_common(struct ebpf_map_array *array_map, uint32_t key_size,
-		      uint32_t value_size, uint32_t max_entries, uint32_t flags)
+array_map_init(struct ebpf_obj *eo)
 {
-	array_map->array = ebpf_calloc(max_entries, key_size);
-	if (!array_map->array) {
+	struct ebpf_map *m = EO2EMAP(eo);
+	void *b;
+
+	b = ebpf_calloc(m->max_entries, m->key_size);
+	if (b == NULL) {
 		return ENOMEM;
 	}
+	BASE_PTR(m) = b;
+	m->percpu = false;
 
 	return 0;
 }
 
 static int
-array_map_init(struct ebpf_map *map, uint32_t key_size, uint32_t value_size,
-	       uint32_t max_entries, uint32_t flags)
+array_map_init_percpu(struct ebpf_obj *eo)
 {
-	int error;
-
-	struct ebpf_map_array *array_map =
-	    ebpf_calloc(1, sizeof(*array_map));
-	if (!array_map) {
-		return ENOMEM;
-	}
-
-	error = array_map_init_common(array_map, key_size, value_size,
-				      max_entries, flags);
-	if (error) {
-		ebpf_free(array_map);
-		return error;
-	}
-
-	map->data = array_map;
-	map->percpu = false;
-
-	return 0;
-}
-
-static int
-array_map_init_percpu(struct ebpf_map *map, uint32_t key_size,
-		      uint32_t value_size, uint32_t max_entries, uint32_t flags)
-{
+	struct ebpf_map *m = EO2EMAP(eo);
 	int error;
 	uint16_t ncpus = ebpf_ncpus();
-
-	struct ebpf_map_array *array_map =
-	    ebpf_calloc(ncpus, sizeof(*array_map));
-	if (!array_map) {
-		return ENOMEM;
-	}
-
 	uint16_t i;
+	void **bp;
+
+	bp = ebpf_calloc(ncpus, sizeof(*bp));
+	if (bp == NULL)
+		return (ENOMEM);
+	BASE_PTR_PERCPU0(m) = bp;
+	EBPF_DPRINTF("%s: alloc %p (m=%p)\n", __func__,
+	    BASE_PTR_PERCPU0(m), m); 
 	for (i = 0; i < ncpus; i++) {
-		error = array_map_init_common(array_map + i, key_size,
-					      value_size, max_entries, flags);
-		if (error) {
+		bp[i] = ebpf_calloc(m->max_entries, m->key_size);
+		if (bp[i] == NULL) {
+			error = ENOMEM;
 			goto err0;
 		}
+		EBPF_DPRINTF("%s: alloc %p (m=%p, i=%d)\n",
+		    __func__, BASE_PTR_PERCPU(m, i), m, i); 
 	}
-
-	map->data = array_map;
-	map->percpu = true;
+	m->percpu = true;
 
 	return 0;
 
 err0:
-	for (uint16_t j = i; j > 0; j--) {
-		ebpf_free(array_map[i].array);
-	}
-
-	ebpf_free(array_map);
+	for (uint16_t j = i; j > 0; j--)
+		ebpf_free(bp[j]);
+	ebpf_free(bp);
 
 	return error;
 }
 
 static void *
-array_map_lookup_elem(struct ebpf_map *map, void *key)
+array_map_lookup_elem(struct ebpf_obj *eo, void *key)
 {
-	uint32_t k = *(uint32_t *)key;
+	struct ebpf_map *m = EO2EMAP(eo);
+	uint32_t k;
+	void *b;
 
-	if (k >= map->max_entries) {
-		return NULL;
-	}
-
-	return (uint8_t *)ARRAY_MAP(map)->array + (map->value_size * k);
+	if (m == NULL || key == NULL)
+		return (NULL);
+	k = *(uint32_t *)key;
+	if (k >= m->max_entries)
+		return (NULL);
+	if (m->percpu)
+		b = BASE_PTR_PERCPU(m, ebpf_curcpu());
+	else
+		b = BASE_PTR(m);
+	EBPF_DPRINTF("%s: slot=%u, value=%u\n", __func__,
+	    *(uint32_t *)key, *(uint32_t *)SLOT_PTR(m, b, k));
+	return SLOT_PTR(m, b, k);
 }
 
 static int
-array_map_lookup_elem_from_user(struct ebpf_map *map, void *key, void *value)
+array_map_lookup_elem_from_user(struct ebpf_obj *eo, void *key,
+    void *value)
 {
-	uint32_t k = *(uint32_t *)key;
+	struct ebpf_map *m = EO2EMAP(eo);
+	uint32_t k;
+	void *elem;
 
-	if (k >= map->max_entries) {
-		return EINVAL;
-	}
+	if (m == NULL || key == NULL)
+		return (EINVAL);
+	k = *(uint32_t *)key;
+	if (k >= m->max_entries)
+		return (EINVAL);
+	elem = array_map_lookup_elem(eo, key);
+	if (elem == NULL)
+		return (ENOENT);
+	if (m->percpu) {
+		uint8_t *v, *v0 = (uint8_t *)value;
 
-	uint8_t *elem =
-	    (uint8_t *)ARRAY_MAP(map)->array + (map->value_size * k);
-	memcpy((uint8_t *)value, elem, map->value_size);
+		for (uint16_t c = 0; c < ebpf_ncpus(); c++) {
+			v = v0 + m->value_size * c;
+			memcpy(v, elem, m->value_size);
+		}
+	} else
+		memcpy(value, elem, m->value_size);
 
-	return 0;
-}
-
-static void *
-array_map_lookup_elem_percpu(struct ebpf_map *map, void *key)
-{
-	uint32_t k = *(uint32_t *)key;
-
-	if (k >= map->max_entries) {
-		return NULL;
-	}
-
-	return (uint8_t *)(ARRAY_MAP(map) + ebpf_curcpu())->array +
-	       (map->value_size * k);
+	return (0);
 }
 
 static int
-array_map_lookup_elem_percpu_from_user(struct ebpf_map *map, void *key,
+array_map_lookup_elem_percpu_from_user(struct ebpf_obj *eo, void *key,
 				       void *value)
 {
-	uint32_t k = *(uint32_t *)key;
 
-	if (k >= map->max_entries) {
-		return EINVAL;
-	}
-
-	uint8_t *elem;
-	for (uint32_t i = 0; i < ebpf_ncpus(); i++) {
-		elem = (uint8_t *)(ARRAY_MAP(map) + i)->array +
-		       (map->value_size * k);
-		memcpy((uint8_t *)value + map->value_size * i, elem,
-		       map->value_size);
-	}
-
-	return 0;
-}
-
-static int
-array_map_update_elem_common(struct ebpf_map *map,
-			     struct ebpf_map_array *array_map, uint32_t key,
-			     void *value, uint64_t flags)
-{
-	uint8_t *elem = (uint8_t *)array_map->array + (map->value_size * key);
-
-	memcpy(elem, value, map->value_size);
-
-	return 0;
+	return array_map_lookup_elem_from_user(eo, key, value);
 }
 
 static inline int
-array_map_update_check_attr(struct ebpf_map *map, void *key, void *value,
+array_map_update_check_attr(struct ebpf_obj *eo, void *key, void *value,
 			    uint64_t flags)
 {
-	if (flags & EBPF_NOEXIST) {
-		return EEXIST;
-	}
+	struct ebpf_map *m = EO2EMAP(eo);
 
-	if (*(uint32_t *)key >= map->max_entries) {
+	if (flags & EBPF_NOEXIST)
+		return (EEXIST);
+
+	if (*(uint32_t *)key >= m->max_entries)
 		return EINVAL;
-	}
 
 	return 0;
 }
 
 static int
-array_map_update_elem(struct ebpf_map *map, void *key, void *value,
+array_map_update_elem0(struct ebpf_obj *eo, void *key, void *value,
+    uint64_t flags, int user)
+{
+	struct ebpf_map *m = EO2EMAP(eo);
+	int error;
+	uint8_t *elem;
+ 
+	EBPF_DPRINTF("%s: enter\n", __func__);
+	error = array_map_update_check_attr(eo, key, value, flags);
+	if (error)
+		return error;
+
+	if (m->percpu) {
+		if (user) {
+			for (uint16_t c = 0; c < ebpf_ncpus(); c++) {
+				elem = SLOT_PTR(m, BASE_PTR_PERCPU(m, c),
+				    *(uint32_t *)key);
+				EBPF_DPRINTF("%s: memcpy value=%u, "
+				    "slot=%u, dst=%p\n", __func__,
+				    *(uint32_t *)value, *(uint32_t *)key,
+				    elem);
+				memcpy(elem, value, m->value_size);
+			}
+		} else  {
+			elem = SLOT_PTR(m, BASE_PTR_PERCPU(m, ebpf_curcpu()),
+			    *(uint32_t *)key);
+			EBPF_DPRINTF("%s: memcpy value=%u, slot=%u, dst=%p\n",
+			    __func__, *(uint32_t *)value, *(uint32_t *)key,
+			    elem);
+			memcpy(elem, value, m->value_size);
+		}
+	} else {
+		elem = SLOT_PTR(m, BASE_PTR(m), *(uint32_t *)key);
+		EBPF_DPRINTF("%s: memcpy value=%u, slot=%u, dst=%p\n",
+		    __func__, *(uint32_t *)value, *(uint32_t *)key, elem);
+		memcpy(elem, value, m->value_size);
+	}
+	EBPF_DPRINTF("%s: leave\n", __func__);
+
+	return (0);
+}
+
+static int
+array_map_update_elem(struct ebpf_obj *eo, void *key, void *value,
 		      uint64_t flags)
 {
-	int error;
-	struct ebpf_map_array *array_map = map->data;
 
-	error = array_map_update_check_attr(map, key, value, flags);
-	if (error) {
-		return error;
-	}
-
-	return array_map_update_elem_common(map, array_map, *(uint32_t *)key,
-					    value, flags);
+	return array_map_update_elem0(eo, key, value, flags, 0);
 }
-
 static int
-array_map_update_elem_percpu(struct ebpf_map *map, void *key, void *value,
-			     uint64_t flags)
+array_map_update_elem_percpu(struct ebpf_obj *eo, void *key, void *value,
+		      uint64_t flags)
 {
-	int error;
-	struct ebpf_map_array *array_map = map->data;
 
-	error = array_map_update_check_attr(map, key, value, flags);
-	if (error) {
-		return error;
-	}
-
-	return array_map_update_elem_common(map, array_map + ebpf_curcpu(),
-					    *(uint32_t *)key, value, flags);
+	return array_map_update_elem0(eo, key, value, flags, 0);
 }
 
 static int
-array_map_update_elem_percpu_from_user(struct ebpf_map *map, void *key,
+array_map_update_elem_percpu_from_user(struct ebpf_obj *eo, void *key,
 				       void *value, uint64_t flags)
 {
-	int error;
-	struct ebpf_map_array *array_map = map->data;
 
-	error = array_map_update_check_attr(map, key, value, flags);
-	if (error) {
-		return error;
-	}
-
-	for (uint16_t i = 0; i < ebpf_ncpus(); i++) {
-		array_map_update_elem_common(map, array_map + i,
-					     *(uint32_t *)key, value, flags);
-	}
-
-	return 0;
+	return array_map_update_elem0(eo, key, value, flags, 1);
 }
 
 static int
-array_map_delete_elem(struct ebpf_map *map, void *key)
+array_map_delete_elem(struct ebpf_obj *eo, void *key)
 {
+
 	return EINVAL;
 }
 
 static int
-array_map_get_next_key(struct ebpf_map *map, void *key, void *next_key)
+array_map_get_next_key(struct ebpf_obj *eo, void *key, void *next_key)
 {
+	struct ebpf_map *m = EO2EMAP(eo);
 	uint32_t k = key ? *(uint32_t *)key : UINT32_MAX;
 	uint32_t *nk = (uint32_t *)next_key;
 
-	if (k >= map->max_entries) {
+	if (k >= m->max_entries) {
 		*nk = 0;
 		return 0;
 	}
 
-	if (k == map->max_entries - 1) {
+	if (k == m->max_entries - 1) {
 		return ENOENT;
 	}
 
@@ -298,15 +282,17 @@ struct ebpf_map_ops array_map_ops = {
     .lookup_elem_from_user = array_map_lookup_elem_from_user,
     .delete_elem_from_user = array_map_delete_elem,
     .get_next_key_from_user = array_map_get_next_key,
-    .deinit = array_map_deinit};
+    .deinit = array_map_deinit,
+};
 
 struct ebpf_map_ops percpu_array_map_ops = {
     .init = array_map_init_percpu,
     .update_elem = array_map_update_elem_percpu,
-    .lookup_elem = array_map_lookup_elem_percpu,
+    .lookup_elem = array_map_lookup_elem,
     .delete_elem = array_map_delete_elem, // delete is anyway invalid
     .update_elem_from_user = array_map_update_elem_percpu_from_user,
     .lookup_elem_from_user = array_map_lookup_elem_percpu_from_user,
     .delete_elem_from_user = array_map_delete_elem, // delete is anyway invalid
     .get_next_key_from_user = array_map_get_next_key,
-    .deinit = array_map_deinit_percpu};
+    .deinit = array_map_deinit_percpu,
+};
