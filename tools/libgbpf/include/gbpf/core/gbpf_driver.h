427d1fd192786c1b5c0ee17020be19a1c4e44fb8
Hiroki Sato <hrs@allbsd.org>
2018-08-01 18:38:59 +0000

    Use typedef'd name.

--- tools/libgbpf/include/gbpf/core/gbpf_driver.h
+++ tools/libgbpf/include/gbpf/core/gbpf_driver.h
@@ -49,17 +49,11 @@ struct gbpf_driver {
 	gbpf_close_map_desc_t *close_map_desc;
 };
 
-int gbpf_load_prog(GBPFDriver *driver, uint16_t prog_type, void *prog,
-		   uint32_t prog_len);
-int gbpf_map_create(GBPFDriver *driver, uint16_t type, uint32_t key_size,
-		    uint32_t value_size, uint32_t max_entries,
-		    uint32_t map_flags);
-int gbpf_map_update_elem(GBPFDriver *driver, int map_desc, void *key,
-			 void *value, uint64_t flags);
-int gbpf_map_lookup_elem(GBPFDriver *driver, int map_desc, void *key,
-			 void *value);
-int gbpf_map_delete_elem(GBPFDriver *driver, int map_desc, void *key);
-int gbpf_map_get_next_key(GBPFDriver *driver, int map_desc, void *key,
-			  void *next_key);
-void gbpf_close_prog_desc(GBPFDriver *driver, int prog_desc);
-void gbpf_close_map_desc(GBPFDriver *driver, int map_desc);
+gbpf_load_prog_t gbpf_load_prog;
+gbpf_map_create_t gbpf_map_create;
+gbpf_map_update_elem_t gbpf_map_update_elem;
+gbpf_map_lookup_elem_t gbpf_map_lookup_elem;
+gbpf_map_delete_elem_t gbpf_map_delete_elem;
+gbpf_map_get_next_key_t gbpf_map_get_next_key;
+gbpf_close_prog_desc_t gbpf_close_prog_desc;
+gbpf_close_map_desc_t gbpf_close_map_desc;
